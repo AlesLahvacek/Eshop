e083161fdf8f571feab52332bd24ad1b81347152
AlesLahvacek <hlavacal21@zaci.spse.cz>
2023-10-10 12:02:17 +0200

    hehe

---
+++ script.js
@@ -0,0 +1,33 @@
+let produkty = document.querySelector(".sell");
+let cena = document.querySelector(".cena");
+let cena2;
+
+
+let cislo = +prompt();
+cena2 = 50 + cislo;
+
+
+
+
+
+
+// for (let i = 0; i < 12; i++) {
+//     cena2 = new cena2[cena.innerHTML];
+    
+// }
+
+console.log(produkty);
+ console.log(cena);
+ console.log(cena2);
+// let value = cena[0];
+// for (let i = 0; i < cena.length; i++) {
+    
+//     if (value < cena) {
+//         value = cena[i];
+//     } else{
+//         let smol = value;
+//         console.log(smol);
+//     }
+    
+// }
+
