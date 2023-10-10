55e00af188b9201dc74d20e3f75661136ba3d70a
AlesLahvacek <hlavacal21@zaci.spse.cz>
2023-10-10 12:24:51 +0200

    hehe2

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 let produkty = document.querySelector(".sell");
-let cena = document.querySelector(".cena");
+let cena = +prompt(document.querySelector(".cena"));
 let cena2;
 
 
 let cislo = +prompt();
-cena2 = 50 + cislo;
+//cena2 = 50 + cislo;
 
 
 
 
 
 
-// for (let i = 0; i < 12; i++) {
-//     cena2 = new cena2[cena.innerHTML];
-    
-// }
+ for (let i = 0; i < 12; i++) {
+     cena2 = new cena2[cena.innerHTML];
+ }
 
 console.log(produkty);
  console.log(cena);
  console.log(cena2);
-// let value = cena[0];
-// for (let i = 0; i < cena.length; i++) {
+ let value = cena[0];
+ for (let i = 0; i < cena.length; i++) {
     
-//     if (value < cena) {
-//         value = cena[i];
-//     } else{
-//         let smol = value;
-//         console.log(smol);
-//     }
+     if (value < cena) {
+         value = cena[i];
+     } else{
+         let smol = value;
+         console.log(smol);
+     }
     
-// }
+ }
 
